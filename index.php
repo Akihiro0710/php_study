10592c511a13ae4e0d9a379de4cd35c546a22b3f
竹内章裕 <ta@takeakiPro.local>
2017-01-16 12:19:25 +0900

    Revert "formのmethodをPOSTに変更"
    This reverts commit dd0514f76b7fb22006a909ed7a57e0ca39281203.

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   <title>PHP study</title>
 </head>
 <body>
-  <form action="" method="POST">
+  <form action="" method="GET">
     <input type="text" name="val">
     <input type="submit">
   </form>
