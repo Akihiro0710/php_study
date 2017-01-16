3b274aef954adcc4a0c67c81f5368cd2d1b8c77f
akihiro0710 <acedia2074@gmail.com>
2017-01-16 14:35:06 +0900

    内容の確認部分をmoduleとして分離

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php include "./commons/header.php";?>
 <?php $val = isset($_POST['val']) ? $_POST['val'] : "";?>
-<form action="" method="POST">
-  <input type="text" name="val" value="<?php echo $val;?>">
-  <input type="submit" name="submit" value="submit">
-</form>
+<?php include "./modules/form.php";?>
+<?php include "./modules/confirm.php";?>
 <?php echo "<p>{$val}</p>";?>
 <?php include "./commons/footer.php";?>
