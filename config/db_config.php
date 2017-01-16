9aed8dc6026887933aadd13e63eb0721a7cf23ce
akihiro0710 <acedia2074@gmail.com>
2017-01-17 02:14:54 +0900

    db設定を別ファイルにし、アクセス不可に設定

---
+++ config/db_config.php
@@ -0,0 +1,8 @@
+<?php
+define('DB_NAME',   'php_study');
+define('DB_HOST',   'localhost');
+define('USERNAME',  'php_study');
+define('PASSWORD',  'password');
+define('CHAR_TYPE', 'utf8');
+define('PDO_DSN',   'mysql:dbname=' . DB_NAME .';host=' . DB_HOST . ';charset=' . CHAR_TYPE);
+?>
--- modules/submit.php
+++ modules/submit.php
@@ -1,16 +1,9 @@
 <?php
 $_SESSION = [];
-session_destroy();
 ?>
 <?php echo $val;?>
 <?php
-define('DB_NAME',   'php_study');
-define('DB_HOST',   'localhost');
-define('USERNAME',  'php_study');
-define('PASSWORD',  'password');
-define('CHAR_TYPE', 'utf8');
-define('PDO_DSN','mysql:dbname=' . DB_NAME .';host=' . DB_HOST . ';charset=' . CHAR_TYPE);
-
+require dirname(__FILE__)."/../config/db_config.php";
 try{
   $pdo = new PDO(PDO_DSN, USERNAME, PASSWORD);
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -20,6 +13,7 @@ try{
   echo "<p>データベースの接続に失敗しました</p>";
 }
 $pdo = null;
+session_destroy();
 ?>
 <div>
   <a href="">home</a>
