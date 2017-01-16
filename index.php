8629818380617cda7de6df048f90539c7e803f80
akihiro0710 <acedia2074@gmail.com>
2017-01-16 14:45:45 +0900

    分離した部分を削除

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
+<?php
+$val = isset($_POST['val']) ? $_POST['val'] : "";
+?>
 <?php include "./commons/header.php";?>
-<?php $val = isset($_POST['val']) ? $_POST['val'] : "";?>
 <?php include "./modules/form.php";?>
 <?php include "./modules/confirm.php";?>
-<?php echo "<p>{$val}</p>";?>
 <?php include "./commons/footer.php";?>
