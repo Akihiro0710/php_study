9a44d3c9c145f7ba9e25c3481b224ba151006003
akihiro0710 <acedia2074@gmail.com>
2017-01-16 14:25:38 +0900

    commonsフォルダに共通のヘッダー、フッターを分離

--- index.php
+++ index.php
@@ -1,15 +1,8 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-  <meta charset="UTF-8">
-  <title>PHP study</title>
-</head>
-<body>
-  <?php $val = isset($_POST['val']) ? $_POST['val'] : "";?>
-  <form action="" method="POST">
-    <input type="text" name="val" value="<?php echo $val;?>">
-    <input type="submit" name="submit" value="submit">
-  </form>
+<?php include "./commons/header.php";?>
+<?php $val = isset($_POST['val']) ? $_POST['val'] : "";?>
+<form action="" method="POST">
+  <input type="text" name="val" value="<?php echo $val;?>">
+  <input type="submit" name="submit" value="submit">
+</form>
 <?php echo "<p>{$val}</p>";?>
-</body>
-</html>
+<?php include "./commons/footer.php";?>
